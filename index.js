79a995307c4c84c9d4a25db0ce997bf846e06714
Eugene Ware <eugene@noblesamurai.com>
2013-07-29 20:48:26 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,186 @@
+var bytewise = require('byteup')(),
+    sublevel = require('level-sublevel'),
+    hooks = require('level-hooks'),
+    through = require('through'),
+    deleteRange = require('level-delete-range');
+
+function encode(key) {
+  return bytewise.encode(key).toString('hex');
+}
+
+function decode(key) {
+  return bytewise.decode(new Buffer(key, 'hex'));
+}
+
+module.exports = levelIndex;
+function levelIndex(db) {
+  db = sublevel(db);
+  hooks(db);
+
+  if (!db.ensureIndex) {
+    db.ensureIndex = ensureIndex.bind(db);
+  }
+
+  if (!db.dropIndex) {
+    db.dropIndex = dropIndex.bind(db);
+  }
+
+  if (!db.getBy) {
+    db.getBy = getBy.bind(db);
+  }
+
+  if (!db.createIndexStream) {
+    db.createIndexStream = createIndexStream.bind(db);
+  }
+
+  if (!db.indexes && !db.indexDb) {
+    db.indexDb = db.sublevel('indexes');
+    db.indexes = {};
+  }
+
+  return db;
+}
+
+function createIndexStream(idxName, options) {
+  var db = this;
+
+  options = options || {};
+  options.start = options.start || [ null ];
+  options.end = options.end || [ undefined ];
+  options.start = encode([idxName].concat(options.start));
+  options.end = encode([idxName].concat(options.end));
+
+  return db.indexDb.createReadStream(options).pipe(through(function (data) {
+    this.queue({ key: decode(data.key), value: data.value });
+  }));
+}
+
+function fetchProp(obj, path) {
+  while (path.length > 0) {
+    var prop = path.shift();
+    if (obj[prop] !== undefined) {
+      obj = obj[prop];
+    } else {
+      return;
+    }
+  }
+  return obj;
+}
+
+function propertyIndex(prop) {
+  return function (key, value, emit) {
+    var val;
+    if (value && prop && (val = fetchProp(value, prop.split('.'))) !== undefined) {
+      if (Array.isArray(val)) {
+        val.forEach(function (item) {
+          emit(item);
+        });
+      } else {
+        emit(val);
+      }
+    }
+  };
+}
+
+function ensureIndex(idxName) {
+  var idxType, emit, cb;
+  var db = this;
+  var args = [].slice.call(arguments).slice(1);
+  var arg = args.shift();
+  if (arg !== undefined && typeof arg === 'string') {
+    idxType = arg;
+    arg = args.shift();
+  }
+
+  if (arg !== undefined && (arg.length > 1 || args.length)) {
+    emit = arg;
+    arg = args.shift();
+  } else {
+    emit = undefined;
+  }
+
+  if (arg !== undefined) {
+    cb = arg;
+  }
+
+  if (emit === undefined) {
+    emit = propertyIndex(idxName);
+    idxType = 'property';
+  }
+  idxType = idxType || 'unspecified';
+  cb = cb || noop;
+
+  var options = {
+    type: idxType,
+    createIndexStream: createIndexStream.bind(db, idxName)
+  };
+  db.indexes[idxName] = options;
+  db.hooks.pre(
+    { start: '\x00', end: '\xFF' },
+    function (change, add, batch) {
+      if (change.type === 'put') {
+        addToIndex(change);
+      } else if (change.type === 'del') {
+        db.get(change.key, function (err, value) {
+          emit(change.key, value, function (valueToIndex) {
+            db.indexDb.del(encode([idxName].concat(valueToIndex).concat(change.key)));
+          });
+        });
+      }
+    });
+
+  var ended = false;
+  var count = 0;
+
+  function addToIndex(dataToIndex, cb) {
+    cb = cb || noop;
+    emit(dataToIndex.key, dataToIndex.value, function (valueToIndex) {
+      count++;
+      db.indexDb.put(encode([idxName].concat(valueToIndex).concat(dataToIndex.key)), dataToIndex.key, function (err) {
+        count--;
+        cb(err);
+      });
+    });
+  }
+
+  db.createReadStream()
+    .on('data', function write(dataToIndex) {
+        addToIndex(dataToIndex, function (err) {
+          if (count === 0 && ended) cb();
+        });
+      })
+    .on('end', function end() {
+        ended = true;
+        if (count === 0) cb();
+    });
+}
+
+function dropIndex(idxName, cb) {
+  cb = cb || function () {};
+  var db = this;
+  deleteRange(db.indexDb, {
+    start: encode([idxName, null]),
+    end: encode([idxName, undefined])
+  }, cb);
+}
+
+function getBy(index, key, cb) {
+  var db = this;
+  if (!Array.isArray(key)) key = [key];
+  var hits = 0;
+  db.createIndexStream(index, { start: key.concat(null), end: key.concat(undefined), limit: 1 })
+    .on('data', function (data) {
+      hits++;
+      db.get(data.value, function (err, value) {
+        cb(err, { key: data.value, value: value });
+      });
+    })
+    .on('error', function (err) {
+      cb(err);
+    })
+    .on('end', function () {
+      if (hits === 0) cb({name: 'NotFoundError', message: 'Could not find value based on key: ' + key.toString()});
+    });
+}
+
+function noop() {}
---
+++ test/subindex.js
@@ -0,0 +1,383 @@
+var expect = require('chai').expect,
+    bytewise = require('byteup')(),
+    levelup = require('levelup'),
+    path = require('path'),
+    subindex = require('../index'),
+    pairs = require('pairs'),
+    rimraf = require('rimraf');
+
+function encode(key) {
+  return bytewise.encode(key).toString('hex');
+}
+
+function decode(key) {
+  return bytewise.decode(new Buffer(key, 'hex'));
+}
+
+function log() {
+  console.error.apply(console, [].slice.apply(arguments));
+}
+
+describe('level-index', function() {
+  var db, dbPath = path.join(__dirname, '..', 'data', 'test-db');
+
+  beforeEach(function(done) {
+    rimraf.sync(dbPath);
+    db = levelup(dbPath, { keyEncoding: 'bytewise', valueEncoding: 'json' }, done);
+  });
+
+  afterEach(function(done) {
+    db.close(done);
+  });
+
+  it('should be able to write to levelup', function(done) {
+    db.batch(testData(), doQuery);
+    function doQuery() {
+      db.get(42, function (err, data) {
+        expect(data.name).to.equal('name 42');
+        done();
+      });
+    }
+  });
+
+  it('should be able to add index functions', function(done) {
+    db = subindex(db);
+    expect(db.ensureIndex).to.be.a('function');
+    expect(db.getBy).to.be.a('function');
+    expect(db.createIndexStream).to.be.a('function');
+    done();
+  });
+
+  it('x should be able to create an index', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('name', function (key, value, emit) {
+      if (value.name !== undefined) emit(value.name);
+    });
+
+    db.batch(testData(), doQuery);
+
+    function doQuery(err) {
+      if (err) return done(err);
+      db.getBy('name', 'name 42', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        done();
+      });
+    }
+  });
+
+  it('should be able to create an index on existing data', function(done) {
+    db = subindex(db);
+
+    db.batch(testData(), function (err) {
+      if (err) return done(err);
+      db.ensureIndex('name', function (key, value, emit) {
+        if (value.name !== undefined) emit(value.name);
+      }, doQuery);
+    });
+
+    function doQuery(err) {
+      if (err) return done(err);
+      db.getBy('name', 'name 42', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        done();
+      });
+    }
+  });
+
+  it('should be able to drop an index', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('name', function (key, value, emit) {
+      if (value.name !== undefined) emit(value.name);
+    });
+
+    db.batch(testData(), doQuery);
+
+    function doQuery(err) {
+      if (err) return done(err);
+      db.getBy('name', 'name 42', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+
+        db.dropIndex('name', doQuery2);
+      });
+    }
+
+    function doQuery2(err) {
+      if (err) return done(err);
+      db.getBy('name', 'name 42', function (err, data) {
+        expect(err.name).to.equal('NotFoundError');
+        done();
+      });
+    }
+  });
+
+  it('should be able to create a property index with no idx function', function(done) {
+    db = subindex(db);
+    db.ensureIndex('name');
+    db.batch(testData(), doQuery);
+
+    function doQuery() {
+      db.getBy('name', 'name 42', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        done();
+      });
+    }
+  });
+
+  it('should be able to index arrays', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('tags', function (key, value, emit) {
+      if (value.tags !== undefined) {
+        if (Array.isArray(value.tags)) {
+          value.tags.forEach(function (tag) {
+            emit(tag);
+          });
+        } else
+          emit(value.tags);
+      }
+    });
+
+    db.batch(testData(), doQuery);
+
+    function doQuery() {
+      db.getBy('tags', 'tag4', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        expect(data.value.tags).to.include('tag4');
+        done();
+      });
+    }
+  });
+
+  it('should be able to handle index deletions', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('name', function (key, value, emit) {
+      if (value.name !== undefined) emit(value.name);
+    });
+
+    db.batch(testData(), doQuery);
+
+    function doQuery() {
+      db.getBy('name', 'name 42', function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        doDelete();
+      });
+    }
+
+    function doDelete() {
+      db.del(42, function (err) {
+        if (err) return done(err);
+        db.getBy('name', 'name 42', function (err, data) {
+          expect(err.name).to.equal('NotFoundError');
+          checkIndex();
+        });
+      });
+    }
+
+    function checkIndex() {
+      db.indexDb.get(encode(['name', 'name 42', 42]), function (err, data) {
+        expect(err.name).to.equal('NotFoundError');
+        done();
+      });
+    }
+  });
+
+  it('should be able to have multiple indexes', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('name', function (key, value, emit) {
+      if (value.name !== undefined) emit(value.name);
+    });
+
+    db.ensureIndex('num', function (key, value, emit) {
+      if (value.num !== undefined) emit(value.num);
+    });
+
+    db.batch(testData(), doQuery);
+
+    function doQuery(err) {
+      if (err) return done(err);
+      db.getBy('name', 'name 42', function (err, data) {
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        doQuery2();
+      });
+    }
+
+    function doQuery2(err) {
+      if (err) return done(err);
+      db.getBy('num', 60, function (err, data) {
+        expect(data.key).to.equal(6);
+        expect(data.value.name).to.equal('name 6');
+        expect(data.value.num).to.equal(60);
+        done();
+      });
+    }
+  });
+
+  it('should be able to create an indexStream', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('name', function (key, value, emit) {
+      if (value.name !== undefined) emit(value.name);
+    });
+
+    db.batch(testData(), doStream);
+
+    function doStream(err) {
+      if (err) return done(err);
+      var num = 0;
+      var is = db.indexes['name'].createIndexStream()
+        .on('data', function (data) {
+          expect(data.key[0]).to.equal('name');
+          expect(data.key[1]).to.equal('name ' + data.value);
+          num++;
+        })
+        .on('end', function () {
+          expect(num).to.equal(99);
+          done();
+        });
+    }
+  });
+
+  it('should be able to index pairs', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('*', 'pairs', function (key, value, emit) {
+      if (value && typeof value === 'object' && Object.keys(value).length > 0) {
+        pairs(value).forEach(function (pair) {
+          emit(pair);
+        });
+      }
+    });
+
+    db.batch(testData());
+
+    setImmediate(function () {
+      db.getBy('*', ['name', 'name 42'], function (err, data) {
+        if (err) return done(err);
+        expect(data.key).to.equal(42);
+        expect(data.value.name).to.equal('name 42');
+        expect(data.value.num).to.equal(420);
+        done();
+      });
+    });
+  });
+
+  it('should be able to create an indexStream from pairs', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('*', function (key, value, emit) {
+      if (value && typeof value === 'object' && Object.keys(value).length > 0) {
+        pairs(value).forEach(function (pair) {
+          emit(pair);
+        });
+      }
+    });
+
+    db.batch(testData(), doStream);
+
+    function doStream(err) {
+      if (err) return done(err);
+      var num = 0;
+      var is = db.createIndexStream('*', {
+          start: ['name', 'name 42', null],
+          end: ['name', 'name 42', undefined]
+        })
+        .on('data', function (data) {
+          expect(data).to.deep.equals(
+            { key: [ '*', 'name', 'name 42', 42 ], value: 42 });
+          num++;
+        })
+        .on('end', function () {
+          expect(num).to.equal(1);
+          done();
+        });
+    }
+  });
+
+  it('should be able to find array items', function(done) {
+    db = subindex(db);
+
+    db.ensureIndex('*', function (key, value, emit) {
+      if (value && typeof value === 'object' && Object.keys(value).length > 0) {
+        pairs(value).forEach(function (pair) {
+          emit(pair);
+        });
+      }
+    });
+
+    db.batch(testData(), doStream);
+
+    function doStream(err) {
+      if (err) return done(err);
+      var num = 0;
+      var is = db.createIndexStream('*', {
+          start: ['tags', 'tag1', null],
+          end: ['tags', 'tag1', undefined]
+        })
+        .on('data', function (data) {
+          expect(data.key[1]).to.equal('tags');
+          expect(data.key[2]).to.equal('tag1');
+          num++;
+        })
+        .on('end', function () {
+          expect(num).to.equal(100);
+          done();
+        });
+    }
+  });
+});
+
+function testData() {
+  var batch = [];
+  for (var i = 0; i < 100; i++) {
+    var obj = {
+      name: 'name ' + i,
+      car: {
+        make: 'Toyota',
+        model: i % 2 ? 'Camry' : 'Corolla',
+        year: 1993 + i
+      },
+      pets: [
+        { species: 'Cat', breed: i == 50 ? 'Saimese' : 'Burmese' },
+        { species: 'Cat', breed: 'DSH' },
+        { species: 'Dog', breed: 'Dalmation' }
+      ],
+      tags: [
+        'tag1', 'tag2', 'tag3'
+      ],
+      num: 10*i
+    };
+    if (i === 42) {
+      obj.tags.push('tag4');
+    }
+    if (i === 84) {
+      delete obj.name;
+    }
+    batch.push({ type: 'put', key: i, value: obj });
+  }
+
+  return batch;
+}
